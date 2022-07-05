74e6f968a5532343143cd49905f8845373aec872
Rudra Sakariya <rudrasakariya16@gmail.com>
2022-07-05 21:28:25 +0530

    ADD: login and detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -131,4 +131,5 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 450px;
 `;
---
+++ src/components/Login.js
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg"></CTALogoOne>
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for an additional fee
+          with a Disney+ subscription. As of 03/26/21, the price of Disney+ and
+          The Disney Bundle will increase by $1.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png"></CTALogoTwo>
+      </CTA>
+    </Container>
+  );
+}
+
+export default Login;
+
+const Container = styled.div`
+  position: relative;
+  height: calc(100vh - 70px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:before {
+    position: absolute;
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: url("/images/login-background.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top;
+    opacity: 0.7;
+    z-index: -1;
+  }
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  padding: 17px 0;
+  color: #f9f9f9;
+  border-radius: 5px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing:1.5px;
+  margin-top: 8px
+
+  &:hover {
+    background: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 90%;
+`;
